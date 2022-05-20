49cfb945a88385e2b07ad56ddc7db09509752f5c
Mayowa <mayobami49@gmail.com>
2022-05-20 12:21:33 +0100

    worked on api documentation

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import indexRouter from './routes/Authentication';
 import propertyRouter from './routes/propertyRoutes';
 
 const app = express();
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,6 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 app.use('/v1', propertyRouter);
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.js'], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use('/hello', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
--- src/routes/Authentication.js
+++ src/routes/Authentication.js
@@ -4,7 +4,79 @@ import { validateUserInput } from '../middleware/middleware';
 
 const indexRouter = express.Router();
 
+/**
+ * @swagger
+ * /signup:
+ *    post:
+ *      summary: Adds a new User.
+ *      description: Retrieve a list of users from JSONPlaceholder.
+ *      responses:
+ *        201:
+ *          description: A list of users
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: integer
+ *                        description: The user ID.
+ *                        example: 1
+ *                      first_name:
+ *                        type: string
+ *                        description: The user's firstName.
+ *                        example: 'Seun'
+ *                      last_name:
+ *                        type: string
+ *                        description: The user's lastName.
+ *                        example: 'Kayode'
+ *                      email:
+ *                        type: string
+ *                        description: The user's email.
+ *                        example: 'Seunkayode@gmail.com'
+ *                      password:
+ *                        type: string
+ *                        description: The user's password.
+ *                        example: 'Seunkayode123'
+ *                      phone_number:
+ *                        type: string
+ *                        description: The user's phoneNumber.
+ *                        example: '0902345673'
+ */
+
 indexRouter.post('/signup', validateUserInput, createAgent);
+
+/**
+ * @swagger
+ * /login:
+ *    post:
+ *      summary: Login in as an agent.
+ *      description: Login as an agent to be able to post, edit, delete property been posted.
+ *      requestBody:
+ *        required: true
+ *      responses:
+ *        200:
+ *          description: User logged in successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  data:
+ *                    type: object
+ *                    properties:
+ *                      id:
+ *                        type: integer
+ *                        description: The user ID.
+ *                        example: 1
+ *                      email:
+ *                        type: string
+ *                        description: The user's email address.
+ *                        example: 'Seunkayode@gmail.com'
+ */
 indexRouter.post('/login', loginAgent);
 
 export default indexRouter;
