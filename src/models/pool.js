5c5463a33e1aeecc0a740b4b96e6e0e7ecf6d3c0
admaya <mayobami49@gmail.com>
2022-08-08 16:13:39 +0100

    worked on pg_hba

--- src/models/pool.js
+++ src/models/pool.js
@@ -1,7 +1,24 @@
-import { Pool } from 'pg';
+import { Pool, Client } from 'pg';
 import dotenv from 'dotenv';
 import { connectionString } from '../settings';
 
 dotenv.config();
 
 export const pool = new Pool({ connectionString });
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  });
