c18304a54d095f6854efb4a6645dabb2ec2cbade
admaya <mayobami49@gmail.com>
2022-08-09 13:18:27 +0100

    worked on my pg config

--- src/models/pool.js
+++ src/models/pool.js
@@ -4,21 +4,28 @@ import { connectionString } from '../settings';
 
 dotenv.config();
 
-export const pool = new Pool({ connectionString });
+// export const pool = new Pool({ connectionString });
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+// client.connect();
+
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//   });
+
+ export const pool = new Pool({
+    connectionString: DATABASE_URL,
     ssl: {
-        rejectUnauthorized: false
+      rejectUnauthorized: false,
     }
 });
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
