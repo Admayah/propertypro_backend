bf22b1c2ac452753664360ac00986a570cf701d9
admaya <mayobami49@gmail.com>
2022-08-11 11:58:53 +0100

    depoly on heroku

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -4,10 +4,10 @@ import fs from 'fs'
 const uploadPropertyImage = async (req, res) => {
     console.log(req.files)
     // console.log(JSON.parse(req.body.json))
-const result = await cloudinary.uploader.upload(req.files.file.tempFilePath, {
+const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
     use_filename: true, folder: 'property-files',
 })
-fs.unlinkSync(req.files.file.tempFilePath)
+fs.unlinkSync(req.files.image.tempFilePath)
 return result.secure_url;
 }
 
