004632e98b842edd6748c31919274586886a698e
admaya <mayobami49@gmail.com>
2022-08-23 14:10:13 +0100

    workd on heroku

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -4,10 +4,10 @@ import fs from 'fs'
 const uploadPropertyImage = async (req, res) => {
     console.log(req.files)
     // console.log(JSON.parse(req.body.json))
-const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+const result = await cloudinary.uploader.upload(req.files.file.tempFilePath, {
     use_filename: true, folder: 'property-files',
 })
-fs.unlinkSync(req.files.image.tempFilePath)
+fs.unlinkSync(req.files.file.tempFilePath)
 return result.secure_url;
 }
 
