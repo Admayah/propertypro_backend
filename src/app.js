5f4a3926ff2dc54bdc7ffa04ca23f826d5665b83
admaya <mayobami49@gmail.com>
2022-08-23 11:37:41 +0100

    workd on heroku

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET
-})
+});
+
 app.use(cors({
   origin: process.env.ORIGIN_URL
 }));
